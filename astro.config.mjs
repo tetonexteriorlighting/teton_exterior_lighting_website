84bdc300050ba0e4955c31bf95622a6ec6bfa801
Teton Exterior Lighting <jayde@tetonexteriorlighting.com>
2026-04-03 20:30:39 -0600

    Add logo, manual sitemap, remove broken sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://tetonexteriorlighting.com',
-  integrations: [sitemap()],
   output: 'static',
 });
